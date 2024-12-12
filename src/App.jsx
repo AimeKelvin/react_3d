af122b9558f2c99e6fd691259c1cce7e6541406a
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-12 21:01:30 +0200

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Projects from "./sections/Projects"
 import Navbar from "./components/Navbar"
 import Footer from "./sections/Footer"
 
-{/*let cursor= document.querySelector('.cursor')
+let cursor= document.querySelector('.cursor')
 let cursor2= document.querySelector('.cursor2')
 
 document.addEventListener("mousemove", function(e){
     cursor.style.cssText =  cursor2.style.cssText = "left: "+ e.clientX + "px; top: "+ e.clientY + "px;";
-})*/}
+})
 
 
 
