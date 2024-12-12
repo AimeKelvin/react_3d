b3cb174ea872c7da5f52873d547908c3e06ece90
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-12 21:11:47 +0200

    cursor cancel

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,6 @@ import Projects from "./sections/Projects"
 import Navbar from "./components/Navbar"
 import Footer from "./sections/Footer"
 
-let cursor= document.querySelector('.cursor')
-let cursor2= document.querySelector('.cursor2')
-
-document.addEventListener("mousemove", function(e){
-    cursor.style.cssText =  cursor2.style.cssText = "left: "+ e.clientX + "px; top: "+ e.clientY + "px;";
-})
-
-
 
 function App() {
 
