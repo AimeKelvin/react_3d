3dcb76245a278da79c5a381fdca699682f1a26e0
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-12 20:50:57 +0200

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import Projects from "./sections/Projects"
 import Navbar from "./components/Navbar"
 import Footer from "./sections/Footer"
 
+{/*let cursor= document.querySelector('.cursor')
+let cursor2= document.querySelector('.cursor2')
+
+document.addEventListener("mousemove", function(e){
+    cursor.style.cssText =  cursor2.style.cssText = "left: "+ e.clientX + "px; top: "+ e.clientY + "px;";
+})*/}
+
+
+
 function App() {
 
     const navbarLinks = [
@@ -19,8 +28,10 @@ function App() {
    <About/>
    <Projects />
    <Experience/>
-
    <Footer/>
+   <div className="cursor"></div>
+   <div className="cursor2"></div>
+   
    </>
   )
 }
